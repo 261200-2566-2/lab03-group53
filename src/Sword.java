759b64d82e2d8f15fe08ab89286e2b6f1b1cc1f2
__.flxk.__ <nitipong4434@gmail.com>
2023-11-29 21:10:36 +0700

    Lab03 TT

--- src/Sword.java
+++ src/Sword.java
@@ -1,19 +1,6 @@
 public class Sword {
-    private int level;
-    private int baseDamage;
-
-    public Sword(int level, int baseDamage) {
-        this.level = level;
-        this.baseDamage = baseDamage;
+    public int SwordLevel;
+    public Sword(int level) {
+        this.SwordLevel = level;
     }
-    public int calculateDamage() {
-        return (int) (baseDamage * (1 + 0.1 * level));
-    }
-    public int getRunSpeedDecrease() {
-        return (int) (10 * (0.1 + 0.04 * level));
-    }
-    public void display(){
-        System.out.println("SwordDamage : " + calculateDamage());
-    }
-
 }
